63f4a5829c75b7ed3020eb1c411d275278cc151d
Sandeep <project.sylllys@gmail.com>
2020-11-03 17:30:55 +1100

    updated api to use zuul api for http redirects

--- src/main/java/sylllys/insideout/InsideoutApplication.java
+++ src/main/java/sylllys/insideout/InsideoutApplication.java
@@ -2,8 +2,10 @@ package sylllys.insideout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class InsideoutApplication {
 
 	public static void main(String[] args) {
--- src/main/java/sylllys/insideout/controllers/InsideOutController.java
+++ src/main/java/sylllys/insideout/controllers/InsideOutController.java
@@ -1,23 +1,19 @@
 package sylllys.insideout.controllers;
 
 import java.util.List;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sylllys.insideout.entities.pojo.SayHello;
 import sylllys.insideout.entities.pojo.ShellCommand;
-import sylllys.insideout.factories.HTTPFactory;
 import sylllys.insideout.factories.ShellFactory;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/insideout")
 public class InsideOutController {
 
   private static final Logger logger = LogManager.getLogger(InsideOutController.class);
@@ -28,18 +24,6 @@ public class InsideOutController {
     logger.info("Received request to say hello");
     return new SayHello();
 
-  }
-
-  @RequestMapping("/http")
-  public void http(HttpServletRequest request, HttpServletResponse response,
-      @RequestHeader("insideout-redirect-url") String redirectURL) {
-
-    logger.info("Received request to redirect url:" + redirectURL);
-
-    HTTPFactory httpFactory = new HTTPFactory(request, response, redirectURL);
-    httpFactory.redirectRequest();
-
-    logger.info("Completed request for redirect url:" + redirectURL);
   }
 
   @PostMapping("/shell")
