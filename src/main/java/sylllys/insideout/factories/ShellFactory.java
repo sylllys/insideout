68b3f0dc8766b894ccde589979f44d5bd98f3660
Sandeep <project.sylllys@gmail.com>
2020-11-23 14:18:46 +1100

    updated code to block chain commands and unbalanced double quotes

--- src/main/java/sylllys/insideout/factories/ShellFactory.java
+++ src/main/java/sylllys/insideout/factories/ShellFactory.java
@@ -3,6 +3,9 @@ package sylllys.insideout.factories;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -65,7 +68,47 @@ public class ShellFactory {
     return false;
   }
 
+  private boolean isChainedCommand(String requestedShellCommandLine) {
+
+    final String regex = "(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)";
+    final String chainOperators = "&&,&,\\|\\|,\\|,!,>,>>,;";
+
+    for (String chainOperator : chainOperators.split(",")) {
+
+      Pattern p = Pattern.compile(chainOperator + regex);
+      Matcher m = p.matcher(requestedShellCommandLine);
+      if (m.find()) {
+        return false;
+      }
+    }
+
+    if (requestedShellCommandLine.trim().endsWith("\\")) {
+      return false;
+    }
+
+    return true;
+  }
+
+  private boolean areDoubleQuotesBalanced(String requestedShellCommandLine) {
+
+    int count = StringUtils.countMatches(requestedShellCommandLine, "\"");
+
+    return count % 2 == 0;
+  }
+
   public void executeCommand(ShellCommand command) {
+
+    if (!areDoubleQuotesBalanced(command.getCommand())) {
+      command.setError("Double quotes are not balanced.");
+      command.setExitCode(3);
+      return;
+    }
+
+    if (!isChainedCommand(command.getCommand())) {
+      command.setError("Chained commands are not supported.");
+      command.setExitCode(3);
+      return;
+    }
 
     if (!isShellCommandAllowed(command.getCommand().split(" ", 2)[0])) {
       command.setError("This command is not allowed, please add this to insideout allowed list.");
